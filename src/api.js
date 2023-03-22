3eef639f5eddda7b2d56b2b8f613b5ad9edddf4a
Billy Lee <codingbillylee@gmail.com>
2023-03-21 18:58:49 -0700

    branch

---
+++ src/api.js
@@ -0,0 +1,30 @@
+import printElements from './index.js';
+
+
+function cryptoInfo(ticker) {
+ let request = new XMLHttpRequest();
+ const url = `https://api.coingecko.com/api/v3/coins/${ticker}`;
+
+ request.addEventListener("loadend", function() {
+  const response = JSON.parse(this.responseText);
+  if (this.status === 200) {
+    console.log(response);
+    printElements(response);
+    
+  // } else {
+  //  handleError(this.statusText);
+  }
+ });
+
+ request.open("GET", url, true);
+
+}
+
+// function handleResponse(response) {
+//  console.log(response);
+// }
+
+// function handleError() {
+//  console.log("request failed");
+// }
+export default cryptoInfo;
